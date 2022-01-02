5d4892d2a7eb1552db3c22f7752cb87a2b9365c2
Maria Lipina <m.o.lip714@gmail.com>
2022-01-02 19:35:39 +0300

    программа + таймер

--- Program.cs
+++ Program.cs
@@ -1,1 +1,50 @@
-﻿
+﻿using System.Diagnostics;
+
+void FillMyArray(int[] collection, int minValue, int maxValue) // А тут войд можно и нужно, потому что метод только заполняет
+{
+    for (int i = 0; i < collection.Length; i++)
+    {
+        collection[i] = new Random().Next(minValue, maxValue + 1);
+    }
+}
+
+string PrintMyArray(int[] collect)
+{
+    string outputString = String.Empty;
+    for (int i = 0; i < collect.Length; i++) outputString += $"{collect[i]} ";
+    return outputString;
+}
+
+int [] OnlyEvens(int[] collect)
+{
+int[] result = new int [collect.Length];
+int futLen = 0;
+
+   for (int i = 0; i < collect.Length; i++)
+    {
+    if (collect[i] % 2 == 0) 
+        {
+        result[futLen] = collect[i];
+        futLen++;
+        }
+    }
+Array.Resize(ref result, futLen);
+return result;
+}
+
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+int [] A = new int [20];
+FillMyArray(A, 0, 100);
+Console.WriteLine("Первоначальный массив");
+Console.WriteLine(PrintMyArray(A));
+
+Console.WriteLine();
+
+int [] B = OnlyEvens(A);
+Console.WriteLine("Итоговый массив");
+Console.WriteLine(PrintMyArray(B));
+
+sw.Stop();
+System.Console.WriteLine(sw.ElapsedMilliseconds);
